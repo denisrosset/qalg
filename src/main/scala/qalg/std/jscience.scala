f3ae95edb6dcb329732a25b00da5e65c56388cd3
Denis Rosset <physics@denisrosset.com>
2015-03-21 14:43:55 +0100

    Finished conversion of JScience.

--- src/main/scala/qalg/std/jscience.scala
+++ src/main/scala/qalg/std/jscience.scala
@@ -106,114 +106,88 @@ final class JScienceLargeIntegerRing extends EuclideanRing[LargeInteger] with Or
   def quot(a: LargeInteger, b: LargeInteger): LargeInteger = a.divide(b)
 }
 
-/*
-object JScienceConv {
-  implicit class LargeIntegerConv(val lhs: LargeInteger) extends AnyVal {
-    def toBigInt: BigInt = ???
-  }
-  implicit class BigIntegerConv(val lhs: BigInt) extends AnyVal {
-    def toLargeInteger: LargeInteger = ???
-  }
+final class JScienceFloat64Vec(implicit val eqA: Eq[Float64], val scalar: Field[Float64]) extends VecInField[Float64Vector, Float64] {
+  type V = Float64Vector
+  type A = Float64
+  def apply(v: V, k: Int) = v.get(k)
+  def length(v: V) = v.getDimension
+  override def plus(x: V, y: V): V = x.plus(y)
+  override def minus(x: V, y: V): V = x.minus(y)
+  override def negate(v: V): V = v.opposite
+  override def timesl(a: Float64, v: V): V = v.times(a)
+  def from(v: FunV[Float64]): V = Float64Vector.valueOf(new java.util.AbstractList[Float64] {
+    def get(k: Int) = v.f(k)
+    def size = v.len
+  })
 }
 
-trait JScienceRational extends Any with JScienceBase[Rational, JRational] {
-  import JScienceConv._
-  def jClassTag: ClassTag[JRational] = implicitly[ClassTag[JRational]]
-  def eqA: Eq[Rational] = implicitly[Eq[Rational]]
-  def scalar: Field[Rational] = implicitly[Field[Rational]]
-  def fromJ(j: JRational): Rational = Rational(j.getDividend.toBigInt, j.getDivisor.toBigInt)
-  def toJ(a: Rational): JRational = JRational.valueOf(a.numerator.toLargeInteger, a.denominator.toLargeInteger)
-}
-
-trait JScienceDouble extends Any with JScienceBase[Double, Float64] {
-  def jClassTag: ClassTag[Float64] = implicitly[ClassTag[Float64]]
-  def eqA: Eq[Double] = implicitly[Eq[Double]]
-  def scalar: Field[Double] = implicitly[Field[Double]]
-  def fromJ(j: Float64): Double = j.doubleValue
-  def toJ(a: Double): Float64 = Float64.valueOf(a)
-}
-
-trait JScienceVec[@sp(Double, Long) A, J <: JField[J], JV <: JVector[J]] extends Any
-    with JScienceBase[A, J]
-    with VecInField[JV, A] {
-
-  def apply(v: JV, k: Int): A = fromJ(v.get(k))
-  def length(v: JV): Int = v.getDimension
-}
-
-trait JScienceMat[@sp(Double, Long) A, J <: JField[J], JM <: JMatrix[J]] extends Any
-    with JScienceBase[A, J]
-    with MatInField[JM, A]
-    with MatInFieldAlg[JM, A] {
-  def fromGenMatrix(m: JMatrix[J]): JM
-  def apply(m: JM, r: Int, c: Int): A = fromJ(m.get(r, c))
-  def nRows(m: JM): Int = m.getNumberOfRows
-  def nCols(m: JM): Int = m.getNumberOfColumns
-  def pinv(m: JM): JM = fromGenMatrix(m.pseudoInverse)
-  def inv(m: JM): JM = fromGenMatrix(m.inverse)
-  def det(m: JM): A = fromJ(m.determinant)
-  def trace(m: JM): A = fromJ(m.trace)
-  override def op(x: JM, y: JM): JM = fromGenMatrix(x.tensor(y))
-  def rref(m: JM): JM = throw new UnsupportedOperationException("rref not implemented")
-  def rank(m: JM): Int = throw new UnsupportedOperationException("rank not implemented")
-}
-
-final class JScienceFloat64MatrixMat extends JScienceMat[Double, Float64, Float64Matrix] with JScienceDouble {
-  def fromGenMatrix(m: JMatrix[Float64]): Float64Matrix = Float64Matrix.valueOf(m)
+final class JScienceFloat64MatVec(implicit val eqA: Eq[Float64], val scalar: Field[Float64], val V: VecInField[Float64Vector, Float64]) extends MatVecInField[Float64Matrix, Float64Vector, Float64] {
+  type M = Float64Matrix
+  type V = Float64Vector
+  type A = Float64
+  def apply(m: M, r: Int, c: Int): A = m.get(r, c)
+  def nRows(m: M): Int = m.getNumberOfRows
+  def nCols(m: M): Int = m.getNumberOfColumns
   override def plus(x: Float64Matrix, y: Float64Matrix): Float64Matrix = x.plus(y)
   override def minus(x: Float64Matrix, y: Float64Matrix): Float64Matrix = x.minus(y)
   override def negate(v: Float64Matrix): Float64Matrix = v.opposite
-  override def timesl(a: Double, v: Float64Matrix): Float64Matrix = v.times(toJ(a))
-  // TODO: optimize conversion
-  def from(m: FunM[Double]): Float64Matrix = Float64Matrix.valueOf(Array.tabulate[Double](m.nR, m.nC)( (r, c) => m.f(r, c)))
+  override def timesl(a: Float64, v: Float64Matrix): Float64Matrix = v.times(a)
+  def from(m: FunM[Float64]): Float64Matrix = Float64Matrix.valueOf(Array.tabulate[Double](m.nR, m.nC)( (r, c) => m.f(r, c).doubleValue))
   override def times(x: Float64Matrix, y: Float64Matrix): Float64Matrix = x.times(y)
-  override def inv(m: Float64Matrix): Float64Matrix = m.inverse
 }
 
-final class JScienceFloat64VectorVec extends JScienceVec[Double, Float64, Float64Vector] with JScienceDouble {
-  override def plus(x: Float64Vector, y: Float64Vector): Float64Vector = x.plus(y)
-  override def minus(x: Float64Vector, y: Float64Vector): Float64Vector = x.minus(y)
-  override def negate(v: Float64Vector): Float64Vector = v.opposite
-  override def timesl(a: Double, v: Float64Vector): Float64Vector = v.times(toJ(a))
-  def from(v: FunV[Double]): Float64Vector = Float64Vector.valueOf(new java.util.AbstractList[Float64] {
-    def get(k: Int) = toJ(v.f(k))
+trait JScienceDenseVectorVec[A <: JField[A]] extends Any
+    with VecInField[JDenseVector[A], A] {
+  type V = JDenseVector[A]
+  implicit def classTagA: ClassTag[A]
+  def apply(v: V, k: Int): A = v.get(k)
+  def length(v: V): Int = v.getDimension
+  def from(v: FunV[A]): JDenseVector[A] = JDenseVector.valueOf(new java.util.AbstractList[A] {
+    def get(k: Int) = v.f(k)
     def size = v.len
   })
+  override def plus(x: V, y: V): V = x.plus(y)
+  override def minus(x: V, y: V): V = x.minus(y)
+  override def negate(v: V): V = v.opposite
+  override def timesl(a: A, v: V): V = v.times(a)
 }
 
-trait JScienceDenseVectorVec[@sp(Double, Long) A, J <: JField[J]] extends Any
-    with JScienceVec[A, J, JDenseVector[J]] {
-  override def plus(x: JDenseVector[J], y: JDenseVector[J]): JDenseVector[J] = x.plus(y)
-  override def minus(x: JDenseVector[J], y: JDenseVector[J]): JDenseVector[J] = x.minus(y)
-  override def negate(v: JDenseVector[J]): JDenseVector[J] = v.opposite
-  override def timesl(a: A, v: JDenseVector[J]): JDenseVector[J] = v.times(toJ(a))
-  def from(v: FunV[A]): JDenseVector[J] = JDenseVector.valueOf(new java.util.AbstractList[J] {
-    def get(k: Int) = toJ(v.f(k))
-    def size = v.len
-  })
+
+trait JScienceDenseMatrixMatVec[A <: JField[A]] extends Any
+    with MatVecInField[JDenseMatrix[A], JDenseVector[A], A] {
+  type M = JDenseMatrix[A]
+  type V = JDenseVector[A]
+  implicit def classTagA: ClassTag[A]
+  def apply(m: M, r: Int, c: Int): A = m.get(r, c)
+  def nRows(m: M): Int = m.getNumberOfRows
+  def nCols(m: M): Int = m.getNumberOfColumns
+  override def plus(x: M, y: M): M = x.plus(y)
+  override def minus(x: M, y: M): M = x.minus(y)
+  override def negate(m: M): M = m.opposite
+  override def timesl(a: A, m: M): M = m.times(a)
+  override def times(x: M, y: M): M = x.times(y)
+  override def t(m: M): M = m.transpose
+  def from(m: FunM[A]): M = JDenseMatrix.valueOf(Array.tabulate[A](m.nR, m.nC)( (r, c) => m.f(r, c)))
 }
 
-trait JScienceDenseMatrixMat[@sp(Double, Long) A, J <: JField[J]] extends Any
-    with JScienceMat[A, J, JDenseMatrix[J]] {
-  def fromGenMatrix(m: JMatrix[J]): JDenseMatrix[J] = JDenseMatrix.valueOf(m)
-
-  override def plus(x: JDenseMatrix[J], y: JDenseMatrix[J]): JDenseMatrix[J] = x.plus(y)
-  override def minus(x: JDenseMatrix[J], y: JDenseMatrix[J]): JDenseMatrix[J] = x.minus(y)
-  override def negate(m: JDenseMatrix[J]): JDenseMatrix[J] = m.opposite
-  override def timesl(a: A, m: JDenseMatrix[J]): JDenseMatrix[J] = m.times(toJ(a))
-  override def times(x: JDenseMatrix[J], y: JDenseMatrix[J]): JDenseMatrix[J] = x.times(y)
-  override def t(m: JDenseMatrix[J]): JDenseMatrix[J] = m.transpose
-  def from(m: FunM[A]): JDenseMatrix[J] = JDenseMatrix.valueOf(Array.tabulate[J](m.nR, m.nC)( (r, c) => toJ(m.f(r, c))))
-  override def inv(m: JDenseMatrix[J]): JDenseMatrix[J] = m.inverse
-}
-
-final class JScienceDenseVectorRationalVec extends JScienceDenseVectorVec[Rational, JRational] with JScienceRational
-
-final class JScienceDenseMatrixRationalMat extends JScienceDenseMatrixMat[Rational, JRational] with JScienceRational
-
-*/
 trait JScienceInstances {
   implicit val JScienceFloat64Field = new JScienceFloat64Field
   implicit val JScienceLargeIntegerRing = new JScienceLargeIntegerRing
   implicit val JScienceRationalField = new JScienceRationalField
+
+  implicit val JScienceRationalDenseVec = new JScienceDenseVectorVec[JRational] {
+    def classTagA = classTag[JRational]
+    def eqA = JScienceRationalField
+    def scalar = JScienceRationalField
+  }
+
+  implicit val JScienceRationalDenseMatVec = new JScienceDenseMatrixMatVec[JRational] {
+    def V = JScienceRationalDenseVec
+    def classTagA = classTag[JRational]
+    def eqA = JScienceRationalField
+    def scalar = JScienceRationalField
+  }
+
+  implicit val JScienceFloat64Vec = new JScienceFloat64Vec
+  implicit val JScienceFloat64MatVec = new JScienceFloat64Vec
 }
